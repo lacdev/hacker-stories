d84de1e2e3e3087505c13f80ba3fdc0a859cfd00
German <german.mtz@protonmail.com>
2021-12-09 10:05:33 -0600

    add List component with map rendering

--- src/App.js
+++ src/App.js
@@ -26,6 +26,25 @@ const list = [
   },
 ]
 
+const List = () => {
+  return (
+    <ul>
+      {list.map((item) => {
+        return (
+          <div key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </div>
+        )
+      })}
+    </ul>
+  )
+}
+
 const App = () => {
   return (
     <div>
@@ -33,21 +52,10 @@ const App = () => {
 
       <label htmlFor="search">Search:</label>
       <input id="search" type="text" />
+
       <hr />
-      <ul>
-        {list.map((item) => {
-          return (
-            <div key={item.objectID}>
-              <span>
-                <a href={item.url}>{item.title}</a>
-              </span>
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-            </div>
-          )
-        })}
-      </ul>
+
+      <List />
     </div>
   )
 }
