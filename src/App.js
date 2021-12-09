72a364941dc8a2ced822e5185f5e0b2231e04d20
German <german.mtz@protonmail.com>
2021-12-09 01:34:39 -0600

    add hacker stories list exercise

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const welcome = {
+  greeting: 'Hey',
+  title: 'React',
 }
 
-export default App;
+const getTitle = (title) => title
+
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+]
+
+const App = () => {
+  return (
+    <div>
+      <h1>My hacker stories</h1>
+
+      <label htmlFor="search">Search:</label>
+      <input id="search" type="text" />
+      <hr />
+      <ul>
+        {list.map((item) => {
+          return (
+            <div key={item.objectID}>
+              <span>
+                <a href={item.url}>{item.title}</a>
+              </span>
+              <span>{item.author}</span>
+              <span>{item.num_comments}</span>
+              <span>{item.points}</span>
+            </div>
+          )
+        })}
+      </ul>
+    </div>
+  )
+}
+
+export default App
