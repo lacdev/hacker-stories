758860afcfc52934662d92224007fdf862ae2112
German <german.mtz@protonmail.com>
2021-12-16 00:31:11 -0600

    add nested destructuring in props item component

--- src/App.js
+++ src/App.js
@@ -43,33 +43,25 @@ const App = () => {
   )
 }
 
-const Search = (props) => {
-  return (
-    <div>
-      <label htmlFor="search">Search:</label>
-      <input
-        id="search"
-        type="text"
-        value={props.search}
-        onChange={props.onSearch}
-      />
-    </div>
-  )
-}
+const Search = ({ search, onSearch }) => (
+  <div>
+    <label htmlFor="search">Search:</label>
+    <input id="search" type="text" value={search} onChange={onSearch} />
+  </div>
+)
 
-const List = ({ list }) => (
-  <ul>
-    {list.map((item) => (
-      <li key={item.objectID}>
-        <span>
-          <a href={item.url}>{item.title}</a>
-        </span>
-        <span>{item.author}</span>
-        <span>{item.num_comments}</span>
-        <span>{item.points}</span>
-      </li>
-    ))}
-  </ul>
+const List = ({ list }) =>
+  list.map((item) => <Item key={item.objectID} item={item} />)
+
+const Item = ({ item: { title, url, author, num_comments, points } }) => (
+  <div>
+    <span>
+      <a href={url}>{title}</a>
+    </span>
+    <span>{author}</span>
+    <span>{num_comments}</span>
+    <span>{points}</span>
+  </div>
 )
 
 export default App
