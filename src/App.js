2700aac9ea1ccc67c5e0cd2f52a99d08df922822
German <german.mtz@protonmail.com>
2021-12-30 00:18:58 -0600

    add server side search feature for search bar component

--- src/App.js
+++ src/App.js
@@ -57,9 +57,10 @@ const App = () => {
   })
 
   React.useEffect(() => {
+    if (!searchTerm) return
     dispatchStories({ type: 'STORIES_FETCH_INIT' })
 
-    fetch(`${API_ENDPOINT}react`)
+    fetch(`${API_ENDPOINT}${searchTerm}`)
       .then((response) => response.json())
       .then((result) => {
         dispatchStories({
@@ -68,7 +69,7 @@ const App = () => {
         })
       })
       .catch(() => dispatchStories({ type: 'STORIES_FETCH_FAILURE' }))
-  }, [])
+  }, [searchTerm])
 
   const handleRemoveStory = (item) => {
     dispatchStories({
@@ -105,7 +106,7 @@ const App = () => {
       {stories.isLoading ? (
         <p>Loading ...</p>
       ) : (
-        <List list={searchedStories} onRemoveItem={handleRemoveStory} />
+        <List list={stories.data} onRemoveItem={handleRemoveStory} />
       )}
     </div>
   )
